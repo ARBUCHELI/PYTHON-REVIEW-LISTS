bf446c5bf6873e9fdc76d036db94478dd2337c07
ARBUCHELI <radio_seattle@hotmail.com>
2023-01-21 12:41:41 -0500

    adding learn_tuples

---
+++ learn_tuples.py
@@ -0,0 +1,18 @@
+my_info = ("Mike", 24, "Programmer")
+print(my_info)
+
+print(my_info[0])
+
+print(my_info[1])
+
+print(my_info[-1])
+
+name, age, occupation = my_info
+
+print(name)
+print(age)
+print(occupation)
+
+one_element_tuple = (4,)
+
+print(one_element_tuple)
